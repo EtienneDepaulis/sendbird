a4e54c697cd1a6550aab8f0dd640c4b92cfc462b
Etienne Depaulis <etienne.depaulis@gmail.com>
2016-01-26 13:43:06 +0100

    Added Messaging#Invite and Messaging#View endpoints - Invite doens't seems to be working properly yet

--- lib/sendbird/messaging.rb
+++ lib/sendbird/messaging.rb
@@ -4,20 +4,74 @@ module Sendbird
     attribute :id, String
     attribute :name, String
     attribute :channel_url, String
+    attribute :last_message, String
+    attribute :last_message_ts, Integer
+    attribute :members, Array[Sendbird::User]
+    attribute :created_at, String
+    attribute :channel_url, String
+
 
 
     def create
+
+      params = {
+        name: name
+      }
+
       response = client.post("#{path}/create", params)
 
       if response.status == 200
         json = JSON.parse response.body
+        json["channel_url"] = json["channel"]["channel_url"]
+
         created_messaging = self.class.new(json)
-        created_messaging.channel_url = json["channel"]["channel_url"]
       else
 
       end
 
       created_messaging
+    end
+
+    def invite(users)
+      users = Array(users)
+
+      params = {
+        channel_url: channel_url,
+        users_ids: users.map(&:id)
+      }
+
+      response = client.post("#{path}/invite", params)
+
+      if response.status == 200
+      else
+
+      end
+
+      self
+    end
+
+    def view
+
+      params = {
+        channel_url: channel_url
+      }
+
+      response = client.post("#{path}/view", params)
+
+      if response.status == 200
+        json = JSON.parse response.body
+
+        json["members"].each do |member|
+          member["user_id"] = member["id"]
+          member["nickame"] = member["name"]
+        end
+
+        element = self.class.new(json)
+      else
+
+      end
+
+      element
     end
 
     private
@@ -31,9 +85,7 @@ module Sendbird
       end
 
       def params
-        {
-          name: name
-        }
+
       end
 
   end
--- spec/messagings_spec.rb
+++ spec/messagings_spec.rb
@@ -4,7 +4,7 @@ describe Sendbird::Messaging do
 
   describe "#create" do
 
-  	let(:messaging) { Sendbird::Messaging.new(name: "Groupe 1") }
+  	let(:messaging) 			{ Sendbird::Messaging.new(name: "Groupe 1") }
 
   	context "valid api token" do
 	    before(:each) do
@@ -15,6 +15,36 @@ describe Sendbird::Messaging do
 
 	    it { expect(@created_messaging).to be_persisted }
 	    it { expect(@created_messaging.channel_url).to eq("jiver_group_messaging_AFD73870-2585-41C9-8E7F-D2A494BEC71A_e1ba77cca364b3d48cc6744550ea798113cf285c") }
+
+  	end
+
+  end
+
+  describe "#invite" do
+
+  	let(:messaging) 		{ Sendbird::Messaging.new(name: "Groupe 1") }
+  	let(:user) 					{ Sendbird::User.new(id: "user_12345", nickname: "Etienne") }
+
+  	context "valid api token" do
+	    before(:each) do
+	    	VCR.use_cassette "messaging_create" do
+	      	@created_messaging = messaging.create
+	      end
+
+	      VCR.use_cassette "user_create" do
+	      	@created_user = user.create
+	      end
+
+	      VCR.use_cassette "messaging_invite" do
+	      	@created_messaging.invite(@created_user)
+	      end
+
+	      VCR.use_cassette "messaging_view" do
+	      	@messaging_with_members = @created_messaging.view
+	      end
+	    end
+
+	    it { expect(@messaging_with_members.members.size).to eq(1) }
 
   	end
 
